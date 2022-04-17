873b5c0704a77201cc786b309bcceeef43a35411
Miroslav <mmurar@scoop-software.cz>
2022-04-17 15:42:43 +0200

    Loaded data throught CommandLineRunner

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -3,12 +3,19 @@ package guru.springframework.sfgpetclinic.services.map;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.CrudService;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import java.util.Set;
 
 /**
  * Created by jt on 7/21/18.
  */
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
+    }
 
     @Override
     public Set<Owner> findAll() {
