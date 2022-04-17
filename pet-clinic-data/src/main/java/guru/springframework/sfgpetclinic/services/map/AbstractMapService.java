32a55bb77ea3ff7572834e30b800bbe641962b42
Miroslav <mmurar@scoop-software.cz>
2022-04-17 17:04:16 +0200

    refractoring, extremely interesting changing Generic Types !

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,16 +1,19 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 /**
  * Created by jt on 7/21/18.
  */
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -20,10 +23,15 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
-
-        return object;
+    T save(T object){
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            return map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object can not be null");
+        }
     }
 
     void deleteById(ID id){
@@ -34,4 +42,15 @@ public abstract class AbstractMapService<T, ID> {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 
+    private Long getNextId() {
+
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
+    }
 }
